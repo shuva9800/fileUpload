a201b8d8eb4e9fe98c10ae0bfc496c7238b755c0
shuva9800 <ffshuva@gmail.com>
2024-05-11 14:02:37 +0530

    mini change

--- routes/cloudrouter.js
+++ routes/cloudrouter.js
@@ -3,11 +3,11 @@ const route = express.Router();
 
 
 const{imageUpload, videoUpload, imagecompressUpload, fileUpload}= require("../controller/fileuploader");
-
 route.post("/fileupload",fileUpload);
 route.post("/imageupload", imageUpload);
- route.post("/videoupload", videoUpload);
+route.post("/videoupload", videoUpload);
 route.post("/compressimg", imagecompressUpload);
+
 
 
 module.exports= route;
